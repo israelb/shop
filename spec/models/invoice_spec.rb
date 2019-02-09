138584ef84e6e72c44f78f05518d00fcd3d23a93
Israel Barba <israelb.aceves@gmail.com>
2019-02-08 19:50:08 -0600

    test nested attributes

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
   it { should have_many(:line_items) }
+  it { should accept_nested_attributes_for(:line_items) }
 
   it { should validate_presence_of(:subtotal) }
   it { should validate_presence_of(:tax) }
